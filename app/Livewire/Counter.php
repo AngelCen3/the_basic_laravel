ad20029c083b371310307247c93b29356295eb68
BetoRobert <wyjawy@hotmail.com>
2023-11-10 11:33:37 -0500

    Mevoy a v3

---
+++ app/Livewire/Counter.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Counter extends Component
+{
+    public $count = 1;
+
+    public function increment($by){
+        $this->count=$this->count+$by;
+    }
+
+    public function render()
+    {
+        return view('livewire.counter', [
+            'count' => $this->count
+        ]);
+    }
+}
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,8 @@
+<div>
+    <!--we declared a varible in counter.php-->
+    Count: {{ $count }}
+
+    <!--We clicked to activate increment function-->
+    <button wire:click="increment(2)">+</button>
+
+</div>
